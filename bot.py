3a69e93cc7aefef6b10ff72f7acc4824e5816c10
docbtc <doug@bitstop.co>
2016-06-22 13:17:10 -0400

    changes for bitstop twitter
    testing by tweeting every 60s

--- bot.py
+++ bot.py
@@ -25,22 +25,28 @@ api = tweepy.API(auth)
 pricing = {}
 locale.setlocale( locale.LC_ALL, '' )
 
+
 if __name__ == "__main__":
-    def check_bullrun():
+    def check_price_difference():
         diff = pricing["second"] - pricing["first"]
-        api.update_status("Bitcoin price difference over the last hour: " + locale.currency(diff) + ".")
-        if (diff > (pricing["second"]/20)):
-            api.update_status("#BITCOINBULLRUN ALERT: The price of Bitcoin is up " + locale.currency(diff) + " (" + str("{:2.2f}".format((diff/pricing["second"]) * 100 )) + "%) over the last hour.")
+        if diff >= 0:
+        api.update_status("The current #bitcoin price is : " + locale.currency(pricing["second"]) + " that is " + str("{:2.2f}".format((diff/pricing["second"]) * 100 ) + "%" + " higher since our last tweet") 
+            )
+        else:
+        api.update_status("The current #bitcoin price is : " + locale.currency(pricing["second"]) + " that is " + str("{:2.2f}".format((diff/pricing["second"]) * 100 ) + "%" + " lower since our last tweet") 
+            )
+
+
     def get_coinbase():
         request = Request('https://api.coinbase.com/v2/prices/spot')
         response = urlopen(request)
         sub = json.loads(response.read())
         return float(sub["data"]["amount"])
 
-    api.update_status("Release the Bulls. Current @coinbase BTC Price: " + locale.currency(get_coinbase()))
+
 
     while True:
         pricing["first"] = get_coinbase()
-        time.sleep(60 * 60) #Tweet every 60 minutes (5 for testing)
+        time.sleep(60) #Tweet every 60 minutes (5 for testing) 
         pricing["second"] = get_coinbase()
-        check_bullrun()
+        check_price_difference()
