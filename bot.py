51e460a130d1e91dc754fa432b00d5d40cef936b
Lazaro Herrera <lazherrera@gmail.com>
2016-07-11 11:54:12 -0400

    Make it 8 hours.

--- bot.py
+++ bot.py
@@ -121,6 +121,6 @@ if __name__ == "__main__":
 
     while True:
         pricing["first"] = get_coinbase()
-        time.sleep(60) #Tweet every 60 minutes (5 for testing) 
+        time.sleep(60*60*8) #Tweet every 60 minutes (5 for testing) 
         pricing["second"] = get_coinbase()
         check_price_difference()
